22568c0973697b1dcab5c5aa141f5677b4811c94
Sophie Handel <sophiehandel@Sophies-MBP.hsd1.ga.comcast.net>
2020-12-17 22:22:57 -0500

    Updates

---
+++ precache-manifest.9bf61accfcdeb754143f13e6dcdeec48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64532d359732f0a2fd22b4aecf1a9ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912ce5e7892967b7fcd5",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912ce5e7892967b7fcd5",
+    "url": "/static/js/main.3c9df0d9.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "5685874fd8163bfe8999b320e4eb9c8c",
+    "url": "/static/media/resume.5685874f.pdf"
+  },
+  {
+    "revision": "b09c67deb23104abe4bd4b53384f2732",
+    "url": "/static/media/sophiahandel.b09c67de.png"
+  }
+]);
